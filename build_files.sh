b4a43974384a9d442e2d768ce7e66cf09c53f48f
dheer152004 <dheerajkumar152004@outlook.com>
2026-02-09 19:14:21 +0530

    DB recreating

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,3 @@ echo "BUILD START"
 python3.10 -m pip install -r requirements-dev.txt
 python3.10 -m manage.py collectstatic --noinput --clear
 echo "BUILD END"
-
-echo "Creating superuser if it doesn't exist..."
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'Password123')
-    print('Superuser created!')
-else:
-    print('Superuser already exists.')
-"
--- pass.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.hashers import make_password
-
-hashed = make_password("MySecretPassword123")
-print(hashed)
